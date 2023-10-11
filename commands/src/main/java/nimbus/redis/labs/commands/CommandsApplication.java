a61fcb69d42970127948f062e5fbca06ef78d5b1
Nimbus <nimbusalex@163.com>
2023-10-11 14:54:18 +0800

    add commands module

---
+++ commands/src/main/java/nimbus/redis/labs/commands/CommandsApplication.java
@@ -0,0 +1,13 @@
+package nimbus.redis.labs.commands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandsApplication.class, args);
+	}
+
+}
---
+++ commands/src/test/java/nimbus/redis/labs/commands/CommandsApplicationTests.java
@@ -0,0 +1,13 @@
+package nimbus.redis.labs.commands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
